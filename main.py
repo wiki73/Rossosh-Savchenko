cbea480165c6294f79dd9acca4e93abe5e75f624
wiki73 <vladimir.savchenko.an@gmail.com>
2024-11-29 20:13:46 +0300

    bla bla bla

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import Qt, QRect
 
-
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -16,20 +15,22 @@ class MyWidget(QMainWindow):
 
     def draw_random_circle(self):
         diameter = random.randint(20, 100)
+
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
 
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.circles.append((x, y, diameter, color))
 
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setBrush(QColor(255, 255, 0))
 
-        for x, y, diameter in self.circles:
+        for x, y, diameter, color in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(QRect(x, y, diameter, diameter))
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
